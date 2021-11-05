75dc8410454dab842ffa6578afe0d59e99e41422
dkash1 <phantom_intel@mail.ru>
2021-11-06 01:56:52 +0500

    git-rebase userebase

--- 02-git-03-branching/branching/rebase.sh
+++ 02-git-03-branching/branching/rebase.sh
@@ -4,9 +4,12 @@
 count=1
 for param in "$@"; do
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     echo "Parameter: $param"
 >>>>>>> d6e7d6f (git-rebase 1)
+=======
+>>>>>>> 69fa066 (git-rebase 1)
     count=$(( $count + 1 ))
 done
 
